aff82ac1fd0d86570e271021e10207956fc4981c
Dan Turner <dan.turner@york.ac.uk>
2014-09-04 17:32:52 +0100

    Getting it working, still horrible performance.

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,41 @@
-import Data.ByteString.Lazy.Char8 hiding (putStrLn)
-import qualified Text.CSV.Lazy.ByteString as CSV
+import Control.DeepSeq
+import Data.ByteString.Lazy as BS hiding (putStrLn, foldl)
+import Data.Char
+import Data.Csv as CSV
+import qualified Data.Vector as V
 import System.Environment
+
 import ShannonEntropyEstimator
 import OptionParser
-import CountingFilter
+import CountingFilter as CF
 
 main = do
 	argv <- getArgs
 	let opts = parseOptions argv
 	case opts of
 		Right o -> do
-			cFilter <- parseCsvToCountingFilter $ optInputFile o
-			print $ estimateEntropy cFilter
-		Left  err -> putStrLn $ show err
+			cFilter <- parse $ optInputFile o
+			case cFilter of
+				Right countingFilter -> print $ estimateEntropy countingFilter
+				Left error -> print error
+		Left  err -> print err
 
-parseCsvToCountingFilter :: FilePath -> IO (CountingFilter ByteString)
-parseCsvToCountingFilter = undefined
+parse :: FilePath -> IO (Either String (CF.CountingFilter BS.ByteString))
+parse filePath = do 
+	inputContents <- BS.readFile filePath
+	case parseCsv inputContents of
+		Right counts ->
+			return $ Right $ V.foldl' (\acc (count, pass) -> whnf $ CF.insertMany pass count acc) CF.empty counts 
+		Left err -> return $ Left err
+
+parseCsvOptions :: DecodeOptions
+parseCsvOptions = defaultDecodeOptions 
+
+parseCsv :: BS.ByteString -> Either String (V.Vector (Integer, BS.ByteString))
+parseCsv = decodeWith parseCsvOptions NoHeader
+
+-- | Evaluates it's argument to weak-head normal form, and returns it.
+whnf a = seq a a
+
+-- | Evaluates it's argument to full normal form, and returns it
+fnf a = deepseq a a
